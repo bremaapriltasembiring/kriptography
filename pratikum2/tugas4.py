216c9d35fce216473a90f941e4f84bdf10f2a212
bremaapriltasembiring <bremasembiring103@gmail.com>
2025-10-24 23:24:44 +0700

    Create tugas4.py

---
+++ pratikum2/tugas4.py
@@ -0,0 +1,16 @@
+text = input("Input Text: ")
+
+print("Teks dari kiri:")
+for i in range(1, len(text) + 1):
+    print(text[:i])
+
+print("\nTeks dari kanan:")
+for i in range(1, len(text) + 1):
+    print(text[-i:])
+
+print("\nTeks dari tengah:")
+middle = len(text) // 2
+for i in range(middle + 1):
+    start = middle - i
+    end = middle + i + 1
+    print(text[start:end])
